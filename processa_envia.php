67774412412aef58186d47a7a22289518bcb9002
Caio <caioo.teless@gmail.com>
2022-02-16 07:39:45 -0300

    Upload com biblioteca

--- processa_envia.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require '../../app_send_mail/libraries/PHPMailer/Exception.php';
-require '../../app_send_mail/libraries/PHPMailer/SMTP.php';
-require '../../app_send_mail/libraries/PHPMailer/OAuth.php';
-require '../../app_send_mail/libraries/PHPMailer/POP3.php';
-require '../../app_send_mail/libraries/PHPMailer/PHPMailer.php';
-require '../../app_send_mail/processa_envia.php';
-?>
